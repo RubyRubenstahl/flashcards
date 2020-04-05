6f0ba3f9d76ed646c5af2e7ead8d20d8fb5e1057
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-04-05 13:45:20 -0400

    Create hex button & group

---
+++ frontend/app/src/components/HexButton.vue
@@ -0,0 +1,46 @@
+<template>
+<div style="filter: drop-shadow(5px 6px 5px rgba(0, 0, 0, 0.9)); ">
+    <q-btn
+        :to="to"
+        class="grow"
+        :style="{backgroundColor: color,width:size, height: size, clipPath: 'polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)'}">
+        <div class="flex flex-center justify-center column q-gutter-md" >
+        <q-icon :name="icon" class="" size="calc(10px + 5vmin)"></q-icon>
+        <span class="text-center text-weight-light hex-text" style="font-size: calc(0.1in + 2vmin)">
+        {{title}}
+        </span>
+        </div>
+
+    </q-btn>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        title:{
+            required: true,
+            type: String
+        },
+        size:{
+            type: String,
+            default: 'calc(1in + 20vmin)'
+        },
+        icon:{
+            type: String
+        },
+        to:{
+            type: String
+        },
+        color:{
+            type: String,
+            default: '#00FFFF22'
+        }
+    }
+}
+</script>
+<style >
+.grow, .q-icon, .hex-text { transition: all .5s ease-in; }
+.grow:hover { transform: scale(1.05)  }
+.grow:hover .q-icon { transform: scale(1.5)  }
+.grow:hover .hex-text { transform: scale(1.2)  }
+</style>
---
+++ frontend/app/src/components/HexButtonGroup.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex flex-center q-gutter-md q-pa-lg">
+        <HexButton v-for="button in buttons" v-bind="button" :key="button.name"/>
+    </div>
+</template>
+<script>
+import HexButton from './HexButton'
+export default {
+    name:'HexButtonGroup',
+    components:{HexButton},
+    props:{
+        buttons:{
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
