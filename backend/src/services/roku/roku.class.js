91197a780b75c9173994410f0dc0d48c49c952c1
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-04-06 15:43:27 -0400

    Implement roku fetching

--- backend/src/services/roku/roku.class.js
+++ backend/src/services/roku/roku.class.js
@@ -1,36 +1,49 @@
 /* eslint-disable no-unused-vars */
+const { Client, keys } = require("roku-client");
+
 exports.Roku = class Roku {
-  constructor (options) {
+  constructor(options) {
     this.options = options || {};
   }
 
-  async find (params) {
+  async find(params) {
     return [];
   }
 
-  async get (id, params) {
-    return {
-      id, text: `A new message with ID: ${id}!`
-    };
+  async get(url, params) {
+    const client = new Client(url);
+    try {
+      const data = {
+        info: await client.info(),
+        apps: await client.apps(),
+        active: await client.active(),
+        
+      };
+    return data;
+
+    } catch (err) {
+      console.err("Failed to get roku data. Reason: " + err.message);
+       return null
+    }
   }
 
-  async create (data, params) {
+  async create(data, params) {
     if (Array.isArray(data)) {
-      return Promise.all(data.map(current => this.create(current, params)));
+      return Promise.all(data.map((current) => this.create(current, params)));
     }
 
     return data;
   }
 
-  async update (id, data, params) {
+  async update(id, data, params) {
     return data;
   }
 
-  async patch (id, data, params) {
+  async patch(id, data, params) {
     return data;
   }
 
-  async remove (id, params) {
+  async remove(id, params) {
     return { id };
   }
 };
--- backend/src/services/tv/tv.hooks.js
+++ backend/src/services/tv/tv.hooks.js
@@ -1,13 +1,20 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
-
+const fetchRokuData = async context => {
+  const { ip, port } = context.data.roku;
+  const rokuUrl = `http://${ip}:${port}`
+  const rokuData = await context.app.service('roku').get(rokuUrl);
+  console.log(rokuData)
+  context.data.roku.data = rokuData;
+  return context;
+}
 module.exports = {
   before: {
     all: [ authenticate('jwt') ],
     find: [],
     get: [],
-    create: [],
+    create: [fetchRokuData],
     update: [],
-    patch: [],
+    patch: [fetchRokuData],
     remove: []
   },
 
