c62308120beedfac5d88b2aef70fddd4faaf966f
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-04-08 11:17:02 -0400

    Styling

--- frontend/app/src/layouts/FormLayout.vue
+++ frontend/app/src/layouts/FormLayout.vue
@@ -10,7 +10,7 @@
           icon="close"
           @click="$router.go(-1)" />
       </q-toolbar>
-     </q-header>m
+     </q-header>
     <q-page-container class="cover ">
       <q-page class="flex content-center column text-center full-width" >
         <h1 class="text-h4" style="opacity:.5"><q-icon :name="$route.meta.icon"></q-icon> {{$route.name}}</h1>
@@ -40,3 +40,16 @@
     }
   }
 </script>
+<style scoped>
+.cover {
+  background-image: linear-gradient(
+      to bottom,
+      rgba(3, 22, 85, 0.95),
+      rgba(14, 11, 100, 0.85)
+      
+    ),
+    url("/statics/menu-background.jpg");
+  background-size: cover;
+  background-position: center top;
+}
+</style>
