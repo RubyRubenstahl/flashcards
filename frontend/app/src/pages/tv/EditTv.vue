c234d87b810a4cdcb303efc952acdf4aa2997b7e
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-04-09 12:15:03 -0400

    Fixed model cloning

--- frontend/app/src/pages/tv/EditTv.vue
+++ frontend/app/src/pages/tv/EditTv.vue
@@ -1,74 +1,80 @@
 <template>
-    <q-form @submit="save" v-if="tv">
-      <div class="q-gutter-md">
-        <q-input
-          autofocus
-          label="Tv Name"
-          v-model="tv.name"
-          class="full-width"
-          filled
-        ></q-input>
-        <q-input
-          label="Icon"
-          v-model="tv.icon"
-          class="full-width"
-          filled
-        ></q-input>
-        <q-input
-          label="Image filename"
-          v-model="tv.imageUrl"
-          class="full-width q-pb-lg"
-          filled
-        ></q-input>
+  <q-form
+    @submit="save"
+    v-if="tv"
+  >
+    <div class="q-gutter-md">
+      <q-input
+        autofocus
+        label="Tv Name"
+        v-model="tv.name"
+        class="full-width"
+        filled
+      ></q-input>
+      <q-input
+        label="Icon"
+        v-model="tv.icon"
+        class="full-width"
+        filled
+      ></q-input>
+      <q-input
+        label="Image filename"
+        v-model="tv.imageUrl"
+        class="full-width q-pb-lg"
+        filled
+      ></q-input>
 
-        <q-input
-          label="Roku IP Address"
-          v-model="tv.roku.ip"
-          class="full-width"
-          filled
-        ></q-input>
-        <q-input
-          label="Roku Port"
-          v-model="tv.roku.port"
-          class="full-width q-pb-lg"
-          filled
-        ></q-input>
+      <q-input
+        label="Roku IP Address"
+        v-model="tv.roku.ip"
+        class="full-width"
+        filled
+      ></q-input>
+      <q-input
+        label="Roku Port"
+        v-model="tv.roku.port"
+        class="full-width q-pb-lg"
+        filled
+      ></q-input>
 
-        <q-input
-          label="LIRC IP Address"
-          v-model="tv.lirc.ip"
-          class="full-width"
-          filled
-        ></q-input>
-        <q-input
-          label="LIRC Port"
-          v-model="tv.roku.port"
-          class="full-width"
-          filled
-        ></q-input>
- 
-        <q-btn
-          type="submit"
-          class="full-width"
-          color="primary"
-        >Save</q-btn>
-        <q-btn
-          class="full-width"
-          color="negative"
-          @click="$router.go(-1)"
-        >Cancel</q-btn>
+      <q-input
+        label="LIRC IP Address"
+        v-model="tv.lirc.ip"
+        class="full-width"
+        filled
+      ></q-input>
+      <q-input
+        label="LIRC Port"
+        v-model="tv.roku.port"
+        class="full-width"
+        filled
+      ></q-input>
 
-        <q-btn
-          class="full-width q-mt-lg"
-          color="negative"
-          @click="remove"
-          v-if="!!$route.params.id"
-          icon="delete"
-          outline
-        >Delete</q-btn>
-      </div>
-    </q-form>
-    <q-spinner-pie v-else size="20vmin" />
+      <q-btn
+        type="submit"
+        class="full-width"
+        color="primary"
+      >Save</q-btn>
+      <q-btn
+        class="full-width"
+        color="negative"
+        @click="$router.go(-1)"
+      >Cancel</q-btn>
+
+      <q-btn
+        class="full-width q-mt-lg"
+        color="negative"
+        @click="remove"
+        v-if="!!$route.params.id"
+        icon="delete"
+        outline
+      >Delete</q-btn>
+    </div>
+  </q-form>
+  <q-spinner-pie
+    v-else
+    size="20vmin"
+  />
 </template>
 
 <script>
@@ -81,31 +87,34 @@
         tv: null
       }
     },
-    created (){
+    created () {
       const { Tv } = this.$FeathersVuex.api
-       if(this.$route.params.id){
-        this.$store.dispatch('tv/get', this.$route.params.id).then(res=>this.tv=res.clone())
-      }else{
-        this.tv = new Tv({ }).clone()
-
+      console.log(Tv)
+      let tv;
+      if (this.$route.params.id) {
+        tv  = Tv.getFromStore(this.$route.params.id)
+      } else {
+        tv = new Tv({}).clone()
       }
-
+      if(tv){
+        this.tv=tv ;
+      }
     },
 
     methods: {
       save () {
-        this.tv.save().then(res=>this.$router.go(-1));
+        this.tv.save().then(res => this.$router.go(-1));
       },
-      remove(){
+      remove () {
         this.$q.dialog({
-        title: 'Confirm',
-        message: `Would you like to delete ${this.tv.name}?`,
-        cancel: true,
-        persistent: true
-      }).onOk(() => {
-        this.tv.remove().then(res=>this.$router.go(-1));
-      })
+          title: 'Confirm',
+          message: `Would you like to delete ${this.tv.name}?`,
+          cancel: true,
+          persistent: true
+        }).onOk(() => {
+          this.tv.remove().then(res => this.$router.go(-1));
+        })
+      }
     }
-  }
   }
 </script>
