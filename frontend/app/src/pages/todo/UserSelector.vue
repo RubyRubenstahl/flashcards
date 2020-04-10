5391914f5cee61aa4a7492bf2287c238e21a90f2
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-04-10 11:04:18 -0400

    Create UserSelector component

---
+++ frontend/app/src/pages/todo/UserSelector.vue
@@ -0,0 +1,49 @@
+<template>
+  <fragment>
+    <q-checkbox
+      v-for="user in users"
+      :key="user._id"
+      :value="value"
+      :val="user._id"
+      :label="user.username"
+      @input="$val=>$emit('input', $val)"
+    />
+  </fragment>
+</template>
+<script>
+  import { mapGetters } from 'vuex'
+  export default {
+    name: 'UserSelector',
+    created(){
+        this.selectCurrentUser();
+        
+    },
+    data () {
+      return {
+          selectedUsers:[],
+          initialized:false
+      }
+    },
+    methods:{
+        selectCurrentUser(){
+            const allUserIds = this.users.map(user=>user._id);
+            // console.log(this.currentUser._)
+            this.$emit('input', [this.currentUser._id])
+        },
+    },
+    computed: {
+      ...mapGetters({
+        users: 'users/list',
+        currentUser: 'auth/user'
+      }),
+    },
+    props: {
+      value: {
+        type: Array
+      }
+    },
+    watch:{
+    }
+  }
+</script>
+
