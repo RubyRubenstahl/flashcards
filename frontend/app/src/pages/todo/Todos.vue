4c83d536755313f6df320739ba7dd7a3b6d34530
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-04-15 13:45:39 -0400

    Add basic todo loading

--- frontend/app/src/pages/todo/Todos.vue
+++ frontend/app/src/pages/todo/Todos.vue
@@ -1,35 +1,73 @@
 <template>
 
   <div style="width:800px">
-    <q-toolbar class="glossy q-mt-lg">
+    <q-toolbar class="glossy q-mt-lg q-mb-md" style="background-color:#00000022">
       <q-space></q-space>
-        <q-btn
-          to="/app/todo/new"
-          label="New"
-        />
+      <q-btn
+        to="/app/todo/new"
+        label="New"
+      />
 
     </q-toolbar>
 
     <q-splitter
       :value="200"
       unit="px"
-       style="height: 400px"
+      style="height: 400px"
     >
       <template v-slot:before>
         <div class="flex column">
-        
-        <div class="text-subtitle1">
-          <q-icon name="fas fa-user" class="q-mr-md"></q-icon>
-          Users
-        </div>
-        <q-separator/>
-        <UserSelector v-model="filters.selectedUsers" />
+
+          <div class="text-subtitle1">
+            <q-icon
+              name="fas fa-user"
+              class="q-mr-md"
+            ></q-icon>
+            Users
+          </div>
+          <q-separator />
+          <UserSelector v-model="filters.selectedUsers" />
         </div>
       </template>
       <template v-slot:after>
-        <div>
-        Hello
-        </div>
+        <FeathersVuexFind
+          service="todos"
+          :query="query"
+          watch="query"
+        >
+          <section
+            class="admin-categories"
+            slot-scope="{ items: todos }"
+          >
+            <q-list>
+              <q-item
+                v-for="todo in todos"
+                :key="todo._id"
+                clickable
+                :to="`/app/todo/edit/${todo._id}`"
+              >
+                <q-item-section>
+                  <q-item-label>{{todo.task}}</q-item-label>
+                  <q-item-label
+                    caption
+                    lines="2"
+                  >{{todo.description}}</q-item-label>
+                </q-item-section>
+
+                <q-item-section
+                  side
+                  top
+                >
+                  <q-item-label caption><q-icon
+                    name="fas fa-user"
+                    size="xs"
+                  /> {{todo.username}}</q-item-label>
+                  
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </section>
+        </FeathersVuexFind>
       </template>
     </q-splitter>
 
@@ -46,7 +84,7 @@
     data () {
       return {
         filters: {
-          selectedUsers: []
+          selectedUsers: [],
         }
       }
     },
