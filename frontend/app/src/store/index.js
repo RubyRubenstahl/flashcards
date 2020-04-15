5f507a840f44443c63fed42f58a095b9ad08bc15
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-04-15 14:44:47 -0400

    Disable strict mode for vuex

--- frontend/app/src/store/index.js
+++ frontend/app/src/store/index.js
@@ -44,7 +44,7 @@ export default function (/* { ssrContext } */) {
 
     // enable strict mode (adds overhead!)
     // for dev mode only
-    strict: process.env.DEV
+    strict: false
   });
   return Store
 }
