afda872d93c679d2e0b464d4d2ea1f1a442a6f3e
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-04-20 12:23:43 -0400

    Remove dead code

--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -1,15 +1,9 @@
 const users = require('./users/users.service.js');
-const rokuKeypress = require('./roku/keypress/keypress.service.js');
-const rokuDevices = require('./roku/devices/devices.service.js');
-const tv = require('./tv/tv.service.js');
-const roku = require('./roku/roku.service.js');
 const todos = require('./todos/todos.service.js');
+const incomingMail = require('./mail/incoming/incoming.service');
+
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
-  app.configure(rokuKeypress);
-  app.configure(rokuDevices);
-  app.configure(tv);
-  app.configure(roku);
   app.configure(todos);
 };
