18aafcf18fb93a262c0f12240ac38517c7359d41
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-04-20 15:54:22 -0400

    Implement outgoing email

--- backend/src/services/mail/outgoing/outgoing.hooks.js
+++ backend/src/services/mail/outgoing/outgoing.hooks.js
@@ -1,11 +1,39 @@
-const { authenticate } = require('@feathersjs/authentication').hooks;
+const { authenticate } = require("@feathersjs/authentication").hooks;
+const sendgrid = require("@sendgrid/mail");
+
+const createMeta = context => {
+  context.data.meta = {
+    sent: false,
+    projectId: null
+  };
+};
+
+const sendEmail = async context => {
+  const settings = context.app.get("sendgrid");
+
+  sendgrid.setApiKey(settings.apiKey);
+  const { to, from, subject, text, html, _id } = context.result;
+  const msg = { to, from, subject, text, html, };
+  try {
+    await sendgrid.send(msg);
+    await context.service.patch(_id, { 'meta.sent': true });
+  } catch (error) {
+    console.error(error);
+    await context.service.patch(_id, { "meta.error": error.message });
+
+    if (error.response) {
+      console.error(error.response.body);
+
+    }
+  }
+};
 
 module.exports = {
   before: {
-    all: [ authenticate('jwt') ],
+    all: [authenticate("jwt")],
     find: [],
     get: [],
-    create: [],
+    create: [createMeta],
     update: [],
     patch: [],
     remove: []
@@ -15,7 +43,7 @@ module.exports = {
     all: [],
     find: [],
     get: [],
-    create: [],
+    create: [sendEmail],
     update: [],
     patch: [],
     remove: []
