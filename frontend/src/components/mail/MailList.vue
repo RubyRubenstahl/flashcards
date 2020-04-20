e0fdfa37e5c67cd4257191354d78ed9410a0c8bf
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-04-20 17:10:22 -0400

    Create email frontend service

---
+++ frontend/src/components/mail/MailList.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-list>
+    <q-item v-if="!emails">
+      <q-item-section >
+        No mail found :(
+      </q-item-section>
+    </q-item>
+
+    <q-item v-else v-for="email in emails" :key ="email._id">
+      <q-item-section >
+        {{email.subject}}
+      </q-item-section>
+    </q-item>
+
+  </q-list>
+</template>
+
+<script>
+export default {
+  // name: 'ComponentName',
+  data () {
+    return {}
+  },
+  props:{
+    emails:{
+      type: [String]
+    }
+  }
+}
+</script>
---
+++ frontend/src/pages/mail/Inbox.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page padding>
+
+    <FeathersVuexFind
+      service="/mail/incoming"
+      :query="{}"
+      watch="query"
+    >
+      <section
+        class="admin-categories"
+        slot-scope="{ items: emails }"
+      >
+        <MailList  :emails="emails"/>
+      </section>
+    </FeathersVuexFind>
+  </q-page>
+</template>
+
+<script>
+  import MailList from 'components/mail/MailList';
+  export default {
+    name: 'PageName',
+    components: { MailList },
+    data(){return {query:{}}}
+  }
+</script>
