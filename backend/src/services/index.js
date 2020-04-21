66337550cd8de396857f7006e0a401512adf7a91
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-04-21 13:29:56 -0400

    Create projects service

--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -4,10 +4,13 @@ const incomingMail = require('./mail/incoming/incoming.service');
 
 const mailOutgoing = require('./mail/outgoing/outgoing.service.js');
 
+const projects = require('./projects/projects.service.js');
+
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(todos);
   app.configure(incomingMail);
   app.configure(mailOutgoing);
+  app.configure(projects);
 };
