e6ac9bd4c51d30ad8ad7c861f8d4bb7e6731dadb
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-04-21 13:20:10 -0400

    Email formatting

--- frontend/src/components/mail/MailList.vue
+++ frontend/src/components/mail/MailList.vue
@@ -1,30 +1,51 @@
 <template>
   <q-list>
     <q-item v-if="!emails">
-      <q-item-section >
+      <q-item-section>
         No mail found :(
       </q-item-section>
     </q-item>
 
-    <q-item v-else v-for="email in emails" :key ="email._id">
-      <q-item-section >
-        {{email.subject}}
-      </q-item-section>
-    </q-item>
+    <q-expansion-item
+      v-else
+      v-for="email in emails"
+      :key="email._id"
+      :label="email.from"
+      :caption="email.subject"
+    >
+      <template v-slot:header>
+        <q-item-section avatar>
+          <q-avatar
+            icon="fas fa-envelope"
+            color="primary"
+            text-color="white"
+          />
+        </q-item-section>
 
+        <q-item-section>
+          <q-item-label>{{email.subject}}</q-item-label>
+          <q-item-label caption>From: {{email.from}}</q-item-label>
+        </q-item-section>
+      </template>
+      <q-card>
+        <q-card-section class="bg-white">
+          <div v-html="email.html"></div>
+        </q-card-section>
+      </q-card>
+    </q-expansion-item>
   </q-list>
 </template>
 
 <script>
-export default {
-  // name: 'ComponentName',
-  data () {
-    return {}
-  },
-  props:{
-    emails:{
-      type: [String]
+  export default {
+    // name: 'ComponentName',
+    data () {
+      return {}
+    },
+    props: {
+      emails: {
+        type: [String]
+      }
     }
   }
-}
 </script>
