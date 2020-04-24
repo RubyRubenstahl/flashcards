3f19ed8e122049a1e77307e6d983da119a40a02b
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-04-24 14:13:59 -0400

    Add projects components

---
+++ frontend/src/pages/contacts/ProjectListPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page padding>
+
+    <ProjectList :projects="projects" />
+  </q-page>
+</template>
+
+<script>
+  import ProjectList from 'components/projects/Projectlist'
+  import { mapActions, mapGetters } from 'vuex';
+  export default {
+    name: 'ProjectListPage',
+    mounted(){
+      this.find();
+    },
+    components: { ProjectList },
+    computed:{
+      ...mapGetters({projects:'projects/list'})
+    },
+    methods:{
+      ...mapActions({find:'projects/find'})
+    },
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
