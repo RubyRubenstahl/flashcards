09e427584102ec32785ae310735c1e5501f173fc
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-04-24 12:34:27 -0400

    Create RichTextEditor

---
+++ frontend/src/components/ui/RichTextEditor.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class=" q-py-lg">
+    <div v-if="label" class="text-title q-pb-sm">{{label}}</div>
+
+    <q-editor
+      v-bind="$attrs"
+      v-on="$listeners"
+      min-height="5rem"
+      :toolbar="[
+              [
+                {
+                  label: $q.lang.editor.align,
+                  icon: $q.iconSet.editor.align,
+                  fixedLabel: true,
+                  options: ['left', 'center', 'right', 'justify']
+                }
+              ],
+              ['bold', 'italic', 'strike', 'underline'],
+              ['token', 'hr', 'link'],
+              
+              [
+                {
+                  icon: $q.iconSet.editor.formatting,
+                  list: 'no-icons',
+                  options: [
+                    'p',
+                    'h1',
+                    'h2',
+                    'h3',
+                    'h4',
+                    'h5',
+                    'h6',
+                    'code'
+                  ]
+                },
+                
+                {
+                  label: 'Font',
+                  icon: $q.iconSet.editor.font,
+                  fixedIcon: true,
+                  list: 'no-icons',
+                  options: [
+                    'default_font',
+                    'arial',
+                    'arial_black',
+                    'comic_sans',
+                    'courier_new',
+                    'impact',
+                    'lucida_grande',
+                    'times_new_roman',
+                    'verdana'
+                  ]
+                },
+                'removeFormat'
+              ],
+              ['quote', 'unordered', 'ordered', 'outdent', 'indent'],
+
+              ['undo', 'redo'],
+              ['viewsource']
+            ]"
+      :fonts="{
+              arial: 'Arial',
+              arial_black: 'Arial Black',
+              comic_sans: 'Comic Sans MS',
+              courier_new: 'Courier New',
+              impact: 'Impact',
+              lucida_grande: 'Lucida Grande',
+              times_new_roman: 'Times New Roman',
+              verdana: 'Verdana'
+            }"
+    />
+  </div>
+</template>
+<script>
+export default {
+    props:{
+        label:{
+            type: String
+        }
+    }
+}
+</script>
+    
