4f0cd18bfb060f3ce98c249b5667f39b4fe94166
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-04-25 02:56:09 -0400

    Create First Run page

---
+++ frontend/src/pages/FirstRun.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page padding>
+      <UserForm mode="new" :value="user"/>
+  </q-page>
+</template>
+
+<script>
+import UserForm from 'components/users/UserForm'
+export default {
+  name: 'FirstRun',
+  components: {UserForm},
+  mounted(){
+    const {User} = this.$FeathersVuex.api;
+    this.user = new User({isAdmin: true, type: 'internal'});
+  },
+  data(){
+    return {
+      user: null,
+      
+    }
+  }
+  
+
+}
+</script>
