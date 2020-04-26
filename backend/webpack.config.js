11f9971608f04797519276e50e66f2aacafab6ec
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-04-26 12:21:26 -0400

    Add webpack backend build

---
+++ backend/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+var nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "src/index.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  target: "node",
+  plugins: [
+    new CopyPlugin([
+      { from: "config", to: "config" },
+      { from: "package.json", to: "package.json" }
+    ])
+  ],
+  externals: [nodeExternals()]
+};
