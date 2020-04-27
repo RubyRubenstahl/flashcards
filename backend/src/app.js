c155efbb4804fd58fada19917efa1ff873c11085
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-04-27 15:11:57 -0400

    Remove favicon from backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -32,8 +32,8 @@ app.use(cors());
 app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
-// Host the public folder
+// app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
+// // Host the public folder
 app.use('/', express.static(app.get('public')));
 
 // Set up Plugins and providers
