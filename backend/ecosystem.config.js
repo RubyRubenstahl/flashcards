843e90a8354d68cf33be14199adef804a0d0be01
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-04-28 15:13:14 -0400

    Create separate pm2 apps for production/dev

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,21 +1,49 @@
 module.exports = {
-  apps : [{
-    name: 'krohtech-partner-manager',
-    script: 'src/index.js',
-  
-    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
+  apps: [
+    {
+      name: "kt-partner-mgr-production",
+      script: "src/index.js",
+      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
     },
-    env_production: {
-      NODE_ENV: 'production'
+    {
+      name: "kt-partner-mgr-staging",
+      script: "src/index.js",
+      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "500M",
+      env: {
+        NODE_ENV: "staging"
+      },
+      env_production: {
+        NODE_ENV: "staging"
+      }
     },
-    env_staging: {
-      NODE_ENV: 'staging'
+    {
+      name: "kt-partner-mgr-development",
+      script: "src/index.js",
+      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "500M",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "development"
+      }
     }
-  }],
+  ]
 };
