712ce65e359292c99eb1db4068dc81baa6cfa93d
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-04-29 03:42:34 -0400

    Add dev badge

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="q-app">
+     <q-badge v-if="process.env.DEV" class="fixed-top-right z-top q-ma-sm" color="red" text-color="white" label="DEV" />
     <router-view />
   </div>
 </template>
