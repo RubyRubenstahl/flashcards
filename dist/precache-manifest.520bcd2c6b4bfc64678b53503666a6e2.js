bdd77ea5185c5fa43ac3e28cbcd6fe1c1c85bf68
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-05-01 04:16:37 -0400

    Add dist

---
+++ dist/precache-manifest.520bcd2c6b4bfc64678b53503666a6e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd2efe629e295e69159",
+    "url": "/css/app.9f3def4a.css"
+  },
+  {
+    "revision": "d5f1b871beca07980e8cc555b459a67d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80e1002f396d919fb1ae",
+    "url": "/js/about.c7a80156.js"
+  },
+  {
+    "revision": "0cd2efe629e295e69159",
+    "url": "/js/app.95a7e107.js"
+  },
+  {
+    "revision": "df5315f896ae44507279",
+    "url": "/js/chunk-vendors.aa4983cc.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
