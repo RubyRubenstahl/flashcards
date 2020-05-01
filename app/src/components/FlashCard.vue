e6d22206a118b4087e057bdae876fb677cf12325
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-04-30 17:13:14 -0700

    Initial commit

---
+++ app/src/components/FlashCard.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="flashcard">
+        <div v-for="(line, index) in inputLines" :key="index">
+            <div v-if="line.type==='value'">{{line.value}}</div>
+        </div>
+
+    </div>
+</template>
+<script>
+export default {
+    name: 'FlashCard',
+    props: {
+        inputLines:{
+            type: Array,
+            required: true
+        },
+        answerLine:{
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+<style scoped>
+    .flashcard{
+       width: 3in;
+       height:5in;
+       box-shadow: 5px 5px 12px #00000088; 
+       background-color: white;
+       border-radius: 12px;
+       padding: 0.5in;
+
+       font-size:1.5in;
+       text-align: right;
+    }
+
+
+</style>
+ 
---
+++ app/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <FlashCard :inputLines="[{type: 'value', value: 4 }, {type: 'value', value: '+8' }]"/>
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+  import FlashCard from '@/components/FlashCard.vue'
+
+  export default {
+    name: 'Home',
+    components: {
+      FlashCard
+    }
+  }
+</script>
+<style scoped>
+.home {
+  width: 100vw ;
+  height: 60vh;
+  color: #2c3e50;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
