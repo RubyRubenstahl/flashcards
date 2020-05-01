76a5eea44a491aabb9f65de50976cec48edb5075
Ruby Rubenstahl <ruby@ruby-lighting.com>
2020-05-01 02:06:00 -0400

    Restructed card input

--- src/components/FlashCard.vue
+++ src/components/FlashCard.vue
@@ -3,30 +3,29 @@
   <div class="container">
     <transition name="fade">
       <div
-        v-if="card.side==='correct'"
+        v-if="isCorrect"
         class="flashcard"
       >
-      Yep</div>
-    </transition>
-    <transition name="fade">
-
+        Yep</div>
       <div
-        v-if="card.side==='incorrect'"
+        v-if="!isCorrect"
         class="flashcard"
-      >Nopes</div>
+      >
+        Nope</div>
     </transition>
+
     <transition name="fade">
 
       <div
         v-if="card.side==='question'"
         class="flashcard flashcard-front"
       >
-        <form @submit.prevent="checkAnswer">
+        <form @submit.prevent="checkAnswer" ref="form">
           <div
             v-for="(row, index) in card.rows"
             :key="index"
           >
-            <FlashCardRow v-model="card.rows[index]"/>
+            <FlashCardRow v-model="card.rows[index]" @submit="card.side='answer'"/>
           </div>
         </form>
       </div>
@@ -40,15 +39,27 @@
     components: { FlashCardRow },
     data () {
       return {
-       card:{
-           rows:[
-               {component: 'number', value: 4},
-               {component: 'input', type: Number, prefix:'+', placeholder: '?', expected: 4},
-               {component: 'divider'},
-               {component: 'number', value: 4}
-               ],
-           side: 'question'
-       }
+        card: {
+          rows: [
+            { component: 'number', value: 4 },
+            { component: 'input', type: Number, prefix: '+', placeholder: '?', expected: 4 },
+            { component: 'divider' },
+            { component: 'number', value: 4 }
+          ],
+          side: 'question'
+        }
+      }
+    },
+    computed: {
+      isCorrect () {
+        const isCorrect = this.card.rows.
+          filter(row => row.component === 'input')
+          .reduce((isCorrect, input) => {
+            if(input.expected===undefined) return true;
+            return isCorrect && input.type(input.value) === input.expected;
+          }, true);
+
+        return isCorrect
       }
     },
     methods: {
@@ -64,17 +75,18 @@
       }
     },
     props: {
-    
+
     }
   }
 </script>
 <style scoped>
-.container{
-    position:fixed;
-    top:1in;
+.container {
+  position: fixed;
+  top: 1in;
 }
 .flashcard {
-    position:fixed;
+  position: fixed;
+  box-sizing: content-box;
   width: 3in;
   height: 5in;
   box-shadow: 5px 5px 12px #00000088;
@@ -98,14 +110,6 @@
   height: 1in;
 }
 
-.divider-line {
-  font-size: 2;
-  border: 4px solid #000000aa;
-  margin: 0;
-  padding: 0;
-  margin-top: 10px;
-}
-
 .fade-enter-active,
 .fade-leave-active {
   transition: transform 0.25s;
@@ -115,25 +119,22 @@
   transition-delay: 0.25s;
 }
 
-
-.fade-leave{
-     transform: rotatey(0deg)
+.fade-leave {
+  transform: rotatey(0deg);
 }
 
 .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-     transform: rotatey(90deg)
+  transform: rotatey(90deg);
 }
 
 .fade-enter /* .fade-leave-active below version 2.1.8 */ {
-     transform: rotatey(-90deg)
+  transform: rotatey(-90deg);
 }
 .fade-enter-to /* .fade-leave-active below version 2.1.8 */ {
-     transform: rotatey(0deg)
+  transform: rotatey(0deg);
 }
 
-
-
-.flashcard{
+.flashcard {
 }
 </style>
  
--- src/components/FlashCardRow.vue
+++ src/components/FlashCardRow.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
+     
     <div v-if="value.component==='number'">{{value.prefix}}{{value.value}}</div>
+    
+    <hr v-if="value.component==='divider'" class="divider-line"/>
+    
     <div v-if="value.component==='input'">
-        {{value.prefix}}
+         {{value.prefix}}
       <input
         v-model="value.value"
         :placeholder="value.placeholder || '?'"
+        class="input-row"
+        :style="{width: String(value.expected).length + 'ch'}"
+        @keypress.enter="$emit('submit')"
       />
     </div>
   </div>
@@ -21,4 +28,17 @@
     }
   }
 </script>
-    
+<style scoped>
+.input-row{
+    font-size: inherit;
+    padding-left:16px;
+}
+
+.divider-line {
+  font-size: 2;
+  border: 4px solid #000000aa;
+  margin: 0;
+  padding: 0;
+  margin-top: 10px;
+}
+</style>        
